fe60d8fae0efdaef156045a966d961ccdcffc3ca
SAURAV <sauravshyju@gmail.com>
2021-10-24 19:01:14 +0530

    Skeleton

---
+++ lib/sideDrawer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class drawerItems extends StatefulWidget {
+  const drawerItems({Key? key}) : super(key: key);
+
+  @override
+  _drawerItemsState createState() => _drawerItemsState();
+}
+
+class _drawerItemsState extends State<drawerItems> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
