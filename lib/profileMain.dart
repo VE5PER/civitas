d31ccd14eff67f9244cbc4025d6c5fa9930b1b8e
Mugunth J C <jc.mugunth@gmail.com>
2021-10-24 19:24:19 +0530

    profile skeleton

---
+++ lib/profileMain.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class profileMainScreen extends StatefulWidget {
+  const profileMainScreen({Key? key}) : super(key: key);
+
+  @override
+  _profileMainScreenState createState() => _profileMainScreenState();
+}
+
+class _profileMainScreenState extends State<profileMainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
