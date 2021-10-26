73c2adb0d1c56a2b958098d521e7ae97ab9ac197
Sudharshan Balaji <cb.en.u4cse19008@cb.students.amrita.edu>
2021-10-26 23:49:03 +0530

    Added map to the Garbage services feature. Modified pubspec.yaml, build.gradle, and AndroidManifest.xml for adding to API key for the Map. Fixed some API issues. :)

--- lib/services/garbageman.dart
+++ lib/services/garbageman.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class garbageman extends StatefulWidget {
   const garbageman({Key? key}) : super(key: key);
@@ -14,7 +15,23 @@ class _garbagemanState extends State<garbageman> {
     return Scaffold(
       appBar: AppBar(
         title: Text("TRASH MAN"),
+        centerTitle: true,
       ),
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            initialCameraPosition: CameraPosition(
+              target: LatLng(13.082680,80.270721),
+              zoom: 12,
+            ),
+          ),
+          Container(
+            alignment: Alignment.bottomCenter,
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
+            child: Text("Just a Prototype!!"),
+          )
+        ],
+      )
     );
   }
 }
