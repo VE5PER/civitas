bbf177f890bfb40ce9e127a20c45e912767c227a
Sudharshan Balaji <cb.en.u4cse19008@cb.students.amrita.edu>
2021-10-26 20:37:52 +0530

    Updated Side Drawer, Ratings Page, Notifications and pubspec

--- lib/ratingpage.dart
+++ lib/ratingpage.dart
@@ -1,4 +1,6 @@
+import 'package:civitas/sidedrawer.dart';
 import 'package:flutter/material.dart';
+import 'package:smooth_star_rating/smooth_star_rating.dart';
 
 class rateus extends StatefulWidget {
   const rateus({Key? key}) : super(key: key);
@@ -8,8 +10,31 @@ class rateus extends StatefulWidget {
 }
 
 class _rateusState extends State<rateus> {
+  var rating=3.0;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Rate Us',style: TextStyle(fontWeight: FontWeight.bold),),
+        centerTitle: true,
+      ),
+      drawer: draweritems(),
+      /*body: Center(
+        child: SmoothStarRating(
+          allowHalfRating: true,
+          onRated: (value){},
+          starCount: 5,
+          rating: rating,
+          size: 40.0,
+          isReadOnly: false,
+          filledIconData: Icons.star_rate_rounded,
+          halfFilledIconData: Icons.star_half_rounded,
+          color: Colors.blueAccent,
+          borderColor: Colors.indigo,
+          spacing: 2.0,
+        ),
+      ),*/
+    );
   }
 }
