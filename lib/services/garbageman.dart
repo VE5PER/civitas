b20a34dffd2901478dab6c7277fdbc74b6da06b9
Sudharshan Balaji <cb.en.u4cse19008@cb.students.amrita.edu>
2021-10-27 15:33:26 +0530

    Added buttons and markers to map. Now when you click on a location, a marker is placed on the location.:)

--- lib/services/garbageman.dart
+++ lib/services/garbageman.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+import 'dart:collection';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class garbageman extends StatefulWidget {
@@ -10,6 +14,31 @@ class garbageman extends StatefulWidget {
 }
 
 class _garbagemanState extends State<garbageman> {
+  Set<Marker> _markers =HashSet<Marker>();
+  late GoogleMapController _mapController;
+  late BitmapDescriptor _markerIcon;
+  List<Marker> myMarker=[];
+
+  void _setMarkerIcon() async{
+    _markerIcon=await BitmapDescriptor.fromAssetImage(ImageConfiguration(), 'assets/ping_icon.png');
+  }
+  void _onMapCreated(GoogleMapController controller){
+    _mapController=controller;
+    setState(() {
+      _markers.add(
+          Marker(
+              markerId: MarkerId("0"),
+              position: LatLng(13.082680,80.270721),
+              infoWindow: InfoWindow(
+                  title: "Chennai",
+                  snippet: "City"
+              ),
+            icon: _markerIcon
+          )
+      );
+    });
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,18 +49,48 @@ class _garbagemanState extends State<garbageman> {
       body: Stack(
         children: <Widget>[
           GoogleMap(
+            onMapCreated: _onMapCreated,
             initialCameraPosition: CameraPosition(
               target: LatLng(13.082680,80.270721),
               zoom: 12,
             ),
+            markers: Set.from(myMarker),
+            onTap: handleTap,
+            //mapType: MapType.hybrid,
+            //markers: _markers,
           ),
           Container(
             alignment: Alignment.bottomCenter,
-            padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
-            child: Text("Just a Prototype!!"),
-          )
+            padding: EdgeInsets.fromLTRB(0, 0, 60, 30),
+            child: FloatingActionButton(
+              backgroundColor: Colors.indigo,
+              onPressed: (){},
+              child: const Icon(Icons.location_on_rounded)
+            ),
+          ),
+          Container(
+            alignment: Alignment.bottomCenter,
+            padding: EdgeInsets.fromLTRB(60, 0, 0, 30),
+            child: FloatingActionButton(
+                backgroundColor: Colors.indigo,
+                onPressed: (){},
+                child: const Icon(Icons.camera_alt_rounded)
+            ),
+          ),
         ],
       )
     );
+  }
+  handleTap(LatLng tappedPoint)
+  {
+    setState(() {
+      myMarker=[];
+      myMarker.add(
+        Marker(
+            markerId: MarkerId(tappedPoint.toString()),
+          position: tappedPoint,
+        )
+      );
+    });
   }
 }
