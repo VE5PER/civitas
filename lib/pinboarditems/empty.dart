346b336dc58630e996c384680ef14376a3b89c28
SAURAV <sauravshyju@gmail.com>
2021-10-28 21:05:59 +0530

    Empty pins theme set

--- lib/pinboarditems/empty.dart
+++ lib/pinboarditems/empty.dart
@@ -14,14 +14,19 @@ class _emptyState extends State<empty> {
     return Container(
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(25),
-            color: Colors.orangeAccent),
+            gradient: LinearGradient(
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
+                colors: [               Color(0xfff83600),
+                  Color(0xfff9d423)])),
         width: MediaQuery.of(context).size.width,
         margin: EdgeInsets.symmetric(horizontal: 5.0, vertical: 20),
+        padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 20),
         child: Center(
           child: Text(
             'Empty Board',
             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
           ),
-        ));
+        ),);
   }
 }
