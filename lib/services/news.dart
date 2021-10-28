1a56c32d869a56eebcca7a63687032bb96ff4af2
Vigneshwararaj <donzdon181201@gmail.com>
2021-10-28 17:30:28 +0530

    News done once run in emulator

--- lib/services/news.dart
+++ lib/services/news.dart
@@ -1,4 +1,5 @@
-import 'package:civitas/services/screen/home.dart';
+
+import 'package:civitas/services/news/home_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -19,20 +20,34 @@ class _newsState extends State<news> {
     );
   }
 }
-
-void main(){
-  runApp(NewsPage());
+void main() {
+  runApp(MyApp());
 }
 
-class NewsPage extends StatelessWidget {
-  
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      title: 'Civitas News',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
     );
-      
-    
   }
 }
+
+      
+    
---
+++ lib/services/news/news_api.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import 'package:civitas/services/news/model.dart';
+
+Future<List<NewsApiModel>> getNews() async {
+  Uri uri = Uri.parse(
+      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=aed2bd3bd2fa4012bea91fe0447cb0d1");
+
+  final response = await http.get(uri);
+
+  if (response.statusCode == 200 || response.statusCode == 201) {
+    Map<String, dynamic> map = json.decode(response.body);
+
+    List _articalsList = map['articles'];
+
+    List<NewsApiModel> newsList = _articalsList
+        .map((jsonData) => NewsApiModel.fromJson(jsonData))
+        .toList();
+
+    print(_articalsList);
+
+    return newsList;
+  } else {
+    print("error");
+    return [];
+  }
+}
