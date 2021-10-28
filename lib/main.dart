dcfd8b98ecd2eab4e5b32b1527b7622c9f526fb0
Sudharshan Balaji <cb.en.u4cse19008@cb.students.amrita.edu>
2021-10-28 19:56:58 +0530

    Added Gradient

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'CIVITAS',
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
