d11c25b47584f9385c7bf0a553343745480ca84c
SAURAV <sauravshyju@gmail.com>
2021-10-31 19:17:47 +0530

    Static notification done

--- lib/notifications.dart
+++ lib/notifications.dart
@@ -17,6 +17,57 @@ class _notificationState extends State<notification> {
             style: TextStyle(fontWeight: FontWeight.bold)),
         centerTitle: true,
       ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 5),
+        child: Column(
+          children: [
+            SizedBox(height: 10,),
+            Card(
+
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  children: [
+                    Icon(Icons.notification_important),
+                    SizedBox(width: 10,),
+                    Container(
+                      width: 250,
+                      child: Text(
+                          "Local garbage reported full. Authorities will take care of it shortly", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                    ),
+                  ],
+                ),
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              color: Colors.grey,
+            ),
+            SizedBox(height: 10,),
+            Card(
+
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  children: [
+                    Icon(Icons.notification_important),
+                    SizedBox(width: 10,),
+                    Container(
+                      width: 250,
+                      child: Text(
+                        "Parking spot marked", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                    ),
+                  ],
+                ),
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              color: Colors.grey,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
